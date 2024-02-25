23d45a159972aad43d5dcff346b33dd8c703ea73
root <leetcipher99@gmail.com>
2024-02-25 13:15:34 -0500

    initial commit

---
+++ self-injection/dll_exports.py
@@ -0,0 +1,14 @@
+import pefile
+import json
+
+exports_list = []
+
+pe = pefile.PE("C:\\Windows\\System32\\kernel32.dll")
+for exp in pe.DIRECTORY_ENTRY_EXPORT.symbols:
+    try:
+        exports_list.append(exp.name.decode("UTF-8"))
+    except:
+        continue
+
+exports_json = {"exports": exports_list}
+open("exports.json", "wb").write(json.dumps(exports_json).encode())
